14a8b3b245eb289f3be139a6afd9a35df157c8cb
Mateusz Podgórski <podgorskima@tlimc.local>
2026-02-19 15:00:07 +0100

    test

---
+++ part2/phonebook/app.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [persons, setPersons] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <ul>
+        {persons.map(person => (
+          <li key={person.id}>{person.name} {person.number}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
